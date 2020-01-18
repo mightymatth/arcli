4644173c74c08a9228e29df80718674531dfe21e
goreleaserbot <goreleaser@carlosbecker.com>
2020-01-18 16:20:27 +0000

    Brew formula update for arcli version v0.1.1

--- arcli.rb
+++ arcli.rb
@@ -2,16 +2,16 @@
 class Arcli < Formula
   desc "Awesome Redmine CLI"
   homepage "https://github.com/mightymatth/arcli"
-  version "0.1.0"
+  version "0.1.1"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/mightymatth/arcli/releases/download/v0.1.0/arcli_Darwin_x86_64.tar.gz"
-    sha256 "c8b8a15f65e84ac5f477f31644f86afb9ad08dd0201dc54c91885d59137c6906"
+    url "https://github.com/mightymatth/arcli/releases/download/v0.1.1/arcli_Darwin_x86_64.tar.gz"
+    sha256 "f6834bc533347211417334f5cf8469797d8f2cc8bfa740757b5691d19f5b514a"
   elsif OS.linux?
     if Hardware::CPU.intel?
-      url "https://github.com/mightymatth/arcli/releases/download/v0.1.0/arcli_Linux_x86_64.tar.gz"
-      sha256 "3700cf5f1242c60842cde5a8bfafc050754e0af40be7621d52ac0434116e2ede"
+      url "https://github.com/mightymatth/arcli/releases/download/v0.1.1/arcli_Linux_x86_64.tar.gz"
+      sha256 "931333803556892e84a68797265d4b8ffe985f62a1df9f9a1e626aacbeff2a1a"
     end
   end
 
